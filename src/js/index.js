a4fe378a401f16743b429a14e07d827b7f8d868f
Vsevolod Kolos <vskolos@icloud.com>
2022-06-16 16:04:42 +0300

    feat: offers section styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
-new Swiper('.swiper', {
+const heroSlider = new Swiper('.swiper.hero__swiper', {
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+  },
+})
+
+const offersSlider = new Swiper('.swiper.offers__swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerGroup: 2,
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    1024: {
+      slidesPerGroup: 3,
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+    1400: {
+      slidesPerGroup: 3,
+      slidesPerView: 'auto',
+      spaceBetween: 32,
+    },
   },
 })
